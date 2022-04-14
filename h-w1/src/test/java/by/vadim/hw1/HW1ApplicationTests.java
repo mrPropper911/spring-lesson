5c1ef4e6d66e7491a67277e15a6e7d224bede58b
Vadim Belyakhovich <vadik123belyahovich@gmail.com>
2022-04-14 22:56:01 +0300

    Inicialise project h-w1

---
+++ h-w1/src/test/java/by/vadim/hw1/HW1ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vadim.hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HW1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
