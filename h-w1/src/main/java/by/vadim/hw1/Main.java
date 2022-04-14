5390bf369bd8a910bca3cd20bdb0869db2aa1896
Vadim Belyakhovich <vadik123belyahovich@gmail.com>
2022-04-14 23:14:38 +0300

    Add context.xml, create structure of project

---
+++ h-w1/src/main/java/by/vadim/hw1/Main.java
@@ -0,0 +1,14 @@
+package by.vadim.hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("/spring-context.xml");
+    }
+
+}
