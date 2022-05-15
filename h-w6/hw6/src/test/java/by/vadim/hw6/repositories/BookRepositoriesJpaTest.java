6baa2db806d6e0a2c25a58c8604dd0904860c857
Vadim Belyakhovich <vadik123belyahovich@gmail.com>
2022-05-15 17:06:31 +0300

    refactor directorys

---
+++ h-w6/hw6/src/test/java/by/vadim/hw6/repositories/BookRepositoriesJpaTest.java
@@ -0,0 +1,37 @@
+package by.vadim.hw6.repositories;
+
+import by.vadim.hw6.models.Book;
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("RepositoryJpa check for work with BOOKS ")
+@DataJpaTest
+class BookRepositoriesJpaTest {
+    private static final long FIRST_BOOK_ID = 1L;
+    private static final String TESTING_TITLE = "Testing title";
+
+    @Autowired
+    private BookRepositoriesJpa repositoryJpa;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @DisplayName("should update title book by her id")
+    @Test
+    void shouldUpdateBookNameById() {
+        val firstBook = testEntityManager.find(Book.class,FIRST_BOOK_ID);
+        String oldTitle = firstBook.getTitle();
+        testEntityManager.detach(firstBook);
+
+        repositoryJpa.updateTitleBook(FIRST_BOOK_ID, TESTING_TITLE);
+        val updateBook = testEntityManager.find(Book.class, FIRST_BOOK_ID);
+
+        assertThat(updateBook.getTitle()).isNotEqualTo(oldTitle).isEqualTo(TESTING_TITLE);
+    }
+}
---
+++ h-w6/hw6/src/test/java/by/vadim/hw6/repositories/BookRepositoriesTest.java
@@ -0,0 +1,32 @@
+package by.vadim.hw6.repositories;
+
+import by.vadim.hw6.models.Book;
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Repository check for work with BOOKS ")
+@DataJpaTest
+class BookRepositoriesTest {
+    private static final long FIRST_BOOK_ID = 1L;
+    private static final String FIRST_BOOK_TITLE = "Дон Кихот";
+
+    @Autowired
+    private BookRepositories bookRepositories;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @DisplayName("should return information about book for her title")
+    @Test
+    void shouldFindExpectedBookByTitle() {
+        val firstBook = testEntityManager.find(Book.class, FIRST_BOOK_ID);
+        Book book = bookRepositories.findByTitle(FIRST_BOOK_TITLE);
+        assertThat(book).isEqualTo(firstBook);
+    }
+}
